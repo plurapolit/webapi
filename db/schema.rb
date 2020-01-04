fa3ff046eb61c9cde9ac7da0facfcf61f4937f01
Robin Zuschke <robin.l.zuschke@gmail.com>
2020-01-04 14:09:42 +0100

    Reset schema and  seed

--- db/schema.rb
+++ db/schema.rb
@@ -14,12 +14,4 @@ ActiveRecord::Schema.define(version: 2019_12_27_183357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "organisations", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-puts 'Start seeding...'
+# puts 'Start seeding...'
 
-puts 'Creating organisations'
-Organisation.create([
-                      { name: 'Die Partei', description: 'Das hier ist ein Parteiname' },
-                      { name: 'Die Grünen', description: 'Auch ein Parteiname' }
-                    ])
+# puts 'Creating organisations'
+# Organisation.create([
+#                       { name: 'Die Partei', description: 'Das hier ist ein Parteiname' },
+#                       { name: 'Die Grünen', description: 'Auch ein Parteiname' }
+#                     ])
 
-puts 'Finished seeding!'
+# puts 'Finished seeding!'
