728d9bedc82eeb3ef66c6d13a51d9827f8104f17
Eddie <eddie.muss@archipinion.com>
2020-01-16 14:19:27 +0100

    Add rack cors to handle cors

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,12 @@ Bundler.require(*Rails.groups)
 module Webapi
   class Application < Rails::Application
     config.load_defaults 6.0
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
   end
 end
