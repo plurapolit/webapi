e72b1f18d7573c4eb9c8e7515963d5ecd26592c5
Robin Zuschke <robin.l.zuschke@gmail.com>
2020-01-16 22:31:15 +0100

    Add syntactical sugar

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Webapi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: %i[get post options]
       end
     end
   end
