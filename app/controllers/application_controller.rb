68c5a41fce4037c4562c2736091ff4edcd87a0ed
Robin Zuschke <robin.l.zuschke@gmail.com>
2020-01-27 20:41:43 +0100

    Implement age range with signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name age_range_id])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name age_range_id])
   end
 end
