c4cd95a6b34501f1dfc295ce05f8bc0d0ae6d216
Robin Zuschke <robin.l.zuschke@gmail.com>
2020-01-28 13:34:31 +0100

    Token with registration

--- app/controllers/api/users/registrations_controller.rb
+++ app/controllers/api/users/registrations_controller.rb
@@ -4,64 +4,37 @@ module Api
   module Users
     class RegistrationsController < Devise::RegistrationsController
       respond_to :json
-      # before_action :configure_sign_up_params, only: [:create]
-      # before_action :configure_account_update_params, only: [:update]
 
-      # GET /resource/sign_up
-      # def new
-      #   super
-      # end
+      # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+      def create
+        build_resource(sign_up_params)
+        resource.save
+        if resource.persisted?
+          if resource.active_for_authentication?
+            sign_up(resource_name, resource)
+            render_create_json
+          else
+            expire_data_after_sign_in!
+            respond_with resource, location: after_inactive_sign_up_path_for(resource)
+          end
+        else
+          clean_up_passwords resource
+          set_minimum_password_length
+          respond_with resource
+        end
+      end
+      # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 
-      # POST /resource
-      # def create
-      #   super
-      # end
+      private
 
-      # GET /resource/edit
-      # def edit
-      #   super
-      # end
+      def current_token
+        request.env['warden-jwt_auth.token']
+      end
 
-      # PUT /resource
-      # def update
-      #   super
-      # end
-
-      # DELETE /resource
-      # def destroy
-      #   super
-      # end
-
-      # GET /resource/cancel
-      # Forces the session data which is usually expired after sign
-      # in to be expired now. This is useful if the user wants to
-      # cancel oauth signing in/up in the middle of the process,
-      # removing all OAuth session data.
-      # def cancel
-      #   super
-      # end
-
-      # protected
-
-      # If you have extra params to permit, append them to the sanitizer.
-      # def configure_sign_up_params
-      #   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-      # end
-
-      # If you have extra params to permit, append them to the sanitizer.
-      # def configure_account_update_params
-      #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-      # end
-
-      # The path used after sign up.
-      # def after_sign_up_path_for(resource)
-      #   super(resource)
-      # end
-
-      # The path used after sign up for inactive accounts.
-      # def after_inactive_sign_up_path_for(resource)
-      #   super(resource)
-      # end
+      def render_create_json
+        render json: { user: resource,
+                       token: current_token }.to_json, status: :created
+      end
     end
   end
 end
