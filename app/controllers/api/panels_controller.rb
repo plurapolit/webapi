95c35a94c0929cb6d82ad11f5b5fe4f50ae7c6cc
Robin Zuschke <robin.l.zuschke@gmail.com>
2020-01-28 23:18:43 +0100

    Remove n+1 queries

--- app/controllers/api/panels_controller.rb
+++ app/controllers/api/panels_controller.rb
@@ -5,25 +5,36 @@ module Api
     respond_to :json
 
     def index
-      @categories = Category.all
+      @categories = Category.all.includes(
+        { panels: [{ avatar_attachment: :blob }] },
+        avatar_attachment: :blob
+      )
     end
 
-    # rubocop:disable Metrics/MethodLength
+    # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
     def show
       @panel = Panel.find(params[:id])
       @category = @panel.category
       @statements_from_experts = @panel.statements
-                                       .includes(:audio_file, :user)
+                                       .includes(
+                                         :audio_file,
+                                         user: [
+                                           { avatar_attachment: :blob },
+                                           { organisation: { avatar_attachment: :blob } }
+                                         ]
+                                       )
                                        .accepted
+                                       .without_comments
                                        .from_experts
                                        .shuffle
 
       @statements_from_community = @panel.statements
                                          .includes(:audio_file)
                                          .accepted
+                                         .without_comments
                                          .from_community
                                          .sorted_by_likes
     end
-    # rubocop:enable Metrics/MethodLength
+    # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
   end
 end
