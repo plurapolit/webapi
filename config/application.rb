4c92421c6bdf11ee554e421666abd0372aeb851c
Robin Zuschke <robin.l.zuschke@gmail.com>
2020-02-04 00:04:56 +0100

    Move active storage down in hierarchy

--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,11 @@ Bundler.require(*Rails.groups)
 
 module Webapi
   class Application < Rails::Application
-    ActiveStorage::Engine.config .active_storage .content_types_to_serve_as_binary .delete('image/svg+xml')
     config.load_defaults 6.0
     Raven.configure do |config|
       config.dsn = Rails.application.credentials.dig(:sentry_dns)
     end
+    ActiveStorage::Engine.config .active_storage .content_types_to_serve_as_binary .delete('image/svg+xml')
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
