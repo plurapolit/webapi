c7b1a324eae4e1356ffa7f6c8b348d1c4e5c231b
Robin Zuschke <robin.l.zuschke@gmail.com>
2020-02-03 23:47:01 +0100

    Serve SVGs

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ Bundler.require(*Rails.groups)
 
 module Webapi
   class Application < Rails::Application
+    ActiveStorage::Engine.config .active_storage .content_types_to_serve_as_binary .delete('image/svg+xml')
     config.load_defaults 6.0
     Raven.configure do |config|
       config.dsn = Rails.application.credentials.dig(:sentry_dns)
